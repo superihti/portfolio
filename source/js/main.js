29f3e27ce96efd407044e4e7dbd5c5d3c6d76322
superihti <superihti@yandex.ru>
2018-04-08 00:40:30 +0300

    day 8

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
   $('.comments-slider').slick({
       dots: true,
-      arrows: true,
-      
-      nextArrow: '<i class="fas fa-angle-right"></i>',
-      prevArrow: '<i class="fas fa-angle-left"></i>'
+      nextArrow: '<i class="fas fa-angle-right comments-fa__next"></i>',
+      prevArrow: '<i class="fas fa-angle-left comments-fa__prev"></i>'
   });
 });
