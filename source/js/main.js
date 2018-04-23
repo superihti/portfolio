87ab3c3cd6f28b8853b44346053e716ed284521a
superihti <superihti@yandex.ru>
2018-04-24 00:39:07 +0300

    day 12

--- source/js/main.js
+++ source/js/main.js
@@ -5,3 +5,29 @@ $(document).ready(function(){
       prevArrow: '<i class="fas fa-angle-left comments-fa__prev"></i>'
   });
 });
+
+// кнопка вверх
+
+$(function() {
+ 
+$(window).scroll(function() {
+ 
+if($(this).scrollTop() != 0) {
+ 
+$('#toTop').fadeIn();
+ 
+} else {
+ 
+$('#toTop').fadeOut();
+ 
+}
+ 
+});
+ 
+$('#toTop').click(function() {
+ 
+$('body,html').animate({scrollTop:0},800);
+ 
+});
+ 
+});
